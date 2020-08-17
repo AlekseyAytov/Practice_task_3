0610fd9d6fc3aa4c59d44cc1e2fb6227d8fea53d
AlekseyAytov <a.aytov@inbox.ru>
2020-08-17 09:13:11 +0300

    add corousel

---
+++ src/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+  $('.carousel__inner').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="img/icons/cup.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="img/icons/heart.png"></button>',
+  });
+});
